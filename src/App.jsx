69d435dc4552bbb3655ea05ef977a24fbf2872f2
TahReEm <tahreemhossain0@gmail.com>
2025-04-20 19:37:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
